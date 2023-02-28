9b469cead44132e5ac63c9694299c4471136cb6f
sergio-prgm <sergio-prgm@outlook.com>
2023-02-28 20:14:20 +0100

    Clean print

--- utils/sql_utils.py
+++ utils/sql_utils.py
@@ -38,9 +38,6 @@ def create_user(db: Session, user: schemas.UserCreate):
     INSERT INTO Users
     VALUES (user.username, user.email...)
     '''
-    # Use password hash thing
-    print(user)
-    # fake_hashed_password = user.password + "almost"
     hashed_password = pwd.get_password_hash(user.password)
     db_user = models.User(email=user.email,
                           hashed_password=hashed_password,
